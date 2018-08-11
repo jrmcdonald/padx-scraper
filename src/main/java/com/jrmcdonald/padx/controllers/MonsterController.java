ea7120c416e0c56847bdda44a42bdb7ee2bb30c9
Jamie McDonald <jamie@qwyck.net>
2018-08-11 01:03:23 +0100

    Added static pages.

--- src/main/java/com/jrmcdonald/padx/controllers/MonsterController.java
+++ src/main/java/com/jrmcdonald/padx/controllers/MonsterController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * MonsterController
  */
 @RestController
+@RequestMapping("/api")
 public class MonsterController {
 
     @Autowired
